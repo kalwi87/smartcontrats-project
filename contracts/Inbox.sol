399ef59bdd22948e3d99a179dbeef6aa174c5cdf
lukasz <biel.lukasz@hotmail.com>
2018-04-22 19:57:30 +0200

    first commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public{
+    }
+    function setMessage(string newMessage)public{
+        message = newMessage;
+    }
+
+}
